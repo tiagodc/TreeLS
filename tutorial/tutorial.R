cb8beccdb526e8c744289a3799bf1522ba90944c
tiagodc <tiagodc_89@yahoo.com.br>
2016-05-24 02:45:52 +0200

    Update tutorial.R

--- tutorial/tutorial.R
+++ tutorial/tutorial.R
@@ -1,4 +1,6 @@
 require(rLiDAR) ; require(rgl)
+source('https://raw.githubusercontent.com/tiagodc/TreeLS/master/stem_extract/base_functions.R')
+source('https://raw.githubusercontent.com/tiagodc/TreeLS/master/stem_extract/pre_sets.R')
 
 path = 'https://raw.githubusercontent.com/tiagodc/TreeLS/master/tutorial/'
 
