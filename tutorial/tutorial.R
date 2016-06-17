ad39c1e0d53d3fcf613c7cb6e88fe0e70ba003dc
tiagodc <tiagodc_89@yahoo.com.br>
2016-06-17 04:10:05 +0200

    Add files via upload

--- tutorial/tutorial.R
+++ tutorial/tutorial.R
@@ -1,4 +1,5 @@
-require(rLiDAR) ; require(rgl)
+if(!require(alphashape3d)) install.packages('alphashape3d')
+
 source('base_functions.R')
 source('pre_sets.R')
 
@@ -18,8 +19,7 @@ M1.trunk = pref_HT(tree = tree, l.int = .5, cell.size = .025, min.den = .3)
 
 M1.stem = fit_RANSAC_circle(trunk = M1.trunk, l.int = .5, cut.rad = .01)
 
-bg3d('black')
-rgl.points(M1.stem[[1]], col=cloud.col(M1.stem[[1]]))
+stem.model(M1.stem)
 rgl.points(M1.trunk, col='darkred')
 rgl.points(tree, col='grey', size=1)
 
@@ -35,8 +35,7 @@ M2.trunk = pref_SD(tree = tree, k = 20, flat.min = .9, ang.tol = 10, l.int = .5,
 
 M2.stem = fit_IRTLS(trunk = M2.trunk, c.len = .5, max.rad = .3, s.height = 1, speed.up = T)
 
-bg3d('black')
-rgl.points(M2.stem[[1]], col=cloud.col(M2.stem[[1]]))
+stem.model(M2.stem)
 rgl.points(M2.trunk, col='darkred')
 rgl.points(tree, col='grey', size=1)
 
@@ -52,8 +51,7 @@ M3.trunk = pref_VN(tree = tree, noise1.rad = .05, noise2.rad = .1, flat.min = .9
 
 M3.stem = fit_RANSAC_cylinder(trunk = M3.trunk, c.len = .5, h.init = 1, max.rad = .3, timesN = 2)
 
-bg3d('black')
-rgl.points(M3.stem[[1]], col=cloud.col(M3.stem[[1]]))
+stem.model(M3.stem)
 rgl.points(M3.trunk, col='darkred')
 rgl.points(tree, col='grey', size=1)
 
