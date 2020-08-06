9ee67cd537b49cf56467c65843a231c4b02bb617
tiagodc <tiagodc_89@yahoo.com.br>
2020-08-06 03:14:06 -0300

    references updated

--- man-roxygen/reference-zhou.R
+++ man-roxygen/reference-zhou.R
@@ -1,2 +1,2 @@
 #' @references
-#' Zhou et al. 2019. Separating leaf and wood points in terrestrial scanning data using multiple optimal scales. Sensors, 19, 1852.
+#' Zhou, J. et. al., 2019. Separating leaf and wood points in terrestrial scanning data using multiple optimal scales. Sensors, 19(8):1852.
--- man-roxygen/section-irls.R
+++ man-roxygen/section-irls.R
@@ -5,6 +5,6 @@
 #' with M-estimators, followed by a Nelder-Mead optimization of squared distance sums
 #' to determine the best circle/cylinder parameters for a given point
 #' cloud. The reweighting strategy used in \emph{TreeLS} is based on 
-#' Liang et al. (2012).The Nelder-Mead algorithm implemented in Rcpp was provided by 
+#' Liang et al. (2012). The Nelder-Mead algorithm implemented in Rcpp was provided by 
 #' \href{https://github.com/kthohr/optim}{kthohr/optim}.
 #' @template reference-liang
